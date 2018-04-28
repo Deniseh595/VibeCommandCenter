fcf86027f84b1cc66be8a421875002aad7e4420c
Deniseh595 <deniseheredia2017@u.northwestern.edu>
2018-04-28 10:01:35 -0700

    Circle menu remote
    New app with HC05, rpi control

---
+++ Remote2/app/src/main/java/vibe/remote2/LEDControl.java
@@ -0,0 +1,294 @@
+package vibe.remote2;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+public class LEDControl extends Activity {
+
+    // GUI Components
+    private TextView mBluetoothStatus;
+    private TextView mReadBuffer;
+    private BluetoothAdapter mBTAdapter;
+    private ListView mDevicesListView;
+    private Button mLEDon;
+    private Button mLEDoff;
+
+    private final String TAG = "bluetoothdebug";
+    private Handler mHandler; // Main handler that will receive callback notifications
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
+
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+    private static String ADDRESS = "98:D3:32:20:C8:7A";
+
+
+    // #defines for identifying shared types between calling functions
+    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    private final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG,"IN ONCREATE...");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBluetoothStatus = (TextView)findViewById(R.id.bluetoothStatus);
+        mReadBuffer = (TextView) findViewById(R.id.readBuffer);
+        mLEDon = (Button)findViewById(R.id.LEDon);
+        mLEDoff = (Button)findViewById(R.id.LEDoff);
+
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter(); // get a handle on the bluetooth radio
+
+
+
+
+
+        mHandler = new Handler(){
+            public void handleMessage(android.os.Message msg){
+                if(msg.what == MESSAGE_READ){
+                    String readMessage = null;
+                    try {
+                        readMessage = new String((byte[]) msg.obj, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                    mReadBuffer.setText(readMessage);
+                }
+
+                if(msg.what == CONNECTING_STATUS){
+                    if(msg.arg1 == 1)
+                        mBluetoothStatus.setText("Connected to Device: " + (String)(msg.obj));
+                    else
+                        mBluetoothStatus.setText("Connection Failed");
+                }
+            }
+        };
+
+        if (mBTAdapter == null) {
+            // Device does not support Bluetooth
+            mBluetoothStatus.setText(R.string.bt_not_found);
+            Log.d(TAG, "BT device not found...");
+        }
+        else {
+
+            mLEDon.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if(mConnectedThread != null) //First check to make sure thread created
+                        mConnectedThread.write("111111");
+                }
+            });
+
+            mLEDoff.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    if (mConnectedThread != null)
+                        mConnectedThread.write("0");
+                }
+            });
+
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG,"..IN ON RESUME..");
+        super.onResume();
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBTAdapter.cancelDiscovery();
+        bluetoothOn();
+        connectDevice();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "..IN ON PAUSE..");
+        try {
+            mBTSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "..IN ON DESTROY..");
+        super.onDestroy();
+        // if (mConnectedThread !=null)     {
+        //     mConnectedThread.cancel();  }
+       // bluetoothOff();
+    }
+
+    private void bluetoothOn(){
+        Log.d(TAG,"..IN BT ON...") ;
+        if (!mBTAdapter.isEnabled()) {
+            //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            mBTAdapter.enable();
+            mBluetoothStatus.setText(R.string.bt_enabled);
+            Log.d(TAG, "Bluetooth enabled..");
+        }
+        else{
+           Log.d(TAG, "Bluetooth already on..");
+        }
+
+    }
+
+
+    private void bluetoothOff(){
+        Log.d(TAG,"..IN BT OFF..");
+        mBTAdapter.disable(); // turn off
+        mBluetoothStatus.setText(R.string.bt_disabled);
+        Log.d(TAG, "BT not on...");
+    }
+
+
+
+    public void connectDevice() {
+
+            if(!mBTAdapter.isEnabled()) {
+                Log.d(TAG, "BT not on..");
+                return;
+            }
+
+            mBluetoothStatus.setText(R.string.connecting);
+
+            new Thread()
+            {
+                public void run() {
+                    boolean fail = false;
+
+                    BluetoothDevice device = mBTAdapter.getRemoteDevice(ADDRESS);
+
+                    try {
+                        mBTSocket = createBluetoothSocket(device);
+                    } catch (IOException e) {
+                        fail = true;
+                        Log.d(TAG, "Socket creation failed...");
+                    }
+                    // Establish the Bluetooth socket connection.
+                    try {
+                        mBTSocket.connect();
+                    } catch (IOException e) {
+                        try {
+                            fail = true;
+                            mBTSocket.close();
+                            mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                    .sendToTarget();
+                        } catch (IOException e2) {
+                            Log.d(TAG, "Socket creation failed..");
+                        }
+                    }
+                    if(!fail) {
+                        mConnectedThread = new ConnectedThread(mBTSocket);
+                        mConnectedThread.start();
+
+                        mHandler.obtainMessage(CONNECTING_STATUS, 1, -1,"HC-05")
+                                .sendToTarget();
+                    }
+                }
+            }.start();
+        }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BTMODULEUUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+        }
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+    public class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true)  {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if(bytes != 0) {
+                        buffer = new byte[1024];
+                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+                        bytes = mmInStream.available(); // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget(); // Send the obtained bytes to the UI activity
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            Log.d(TAG,"SENDING VALUE:" + input);
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { e.printStackTrace();}
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { e.printStackTrace(); }
+        }
+    }
+
+
+}
---
+++ Remote2/app/src/main/java/vibe/remote2/MenuActivity.java
@@ -0,0 +1,89 @@
+package vibe.remote2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+
+import static java.lang.Thread.sleep;
+
+
+public class MenuActivity extends AppCompatActivity {
+
+
+    private CircleMenu circleMenu;
+    private Handler mHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        circleMenu = findViewById(R.id.circle_menu);
+        circleMenu.setMainMenu(Color.parseColor("#ffffff"),R.mipmap.ic_vibe3,R.mipmap.ic_vibe3)
+        .addSubMenu(Color.parseColor("#ffffff"),R.mipmap.ic_lightbulb)
+                .addSubMenu(Color.parseColor("#ffffff"),R.mipmap.ic_chameleon);
+
+
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        circleMenu.openMenu();
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        circleMenu.closeMenu();
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        circleMenu.setOnMenuSelectedListener(new OnMenuSelectedListener() {
+
+            @Override
+            public void onMenuSelected(int i) {
+                if (i==0) {
+                    mHandler.postDelayed(mUpdateTimeTask, 900);
+
+                }
+                if (i==1) {
+                    /*CharSequence text = "Send to Chameleon control..";
+                    Toast toast = Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT);
+                    toast.show();*/
+                    mHandler.postDelayed(mUpdateTimeTask2,900);
+                }
+            }
+
+            private Runnable mUpdateTimeTask = new Runnable() {
+                public void run() {
+                    Intent ledIntent = new Intent(MenuActivity.this,
+                            LEDControl.class);
+                    startActivity(ledIntent);
+                }
+            };
+
+            private Runnable mUpdateTimeTask2 = new Runnable() {
+                @Override
+                public void run() {
+                    Intent chamIntent = new Intent(MenuActivity.this,Chameleon.class);
+                    startActivity(chamIntent);
+                }
+            };
+
+        });
+    }
+
+
+}
